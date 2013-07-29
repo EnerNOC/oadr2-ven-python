21f523f7e9db789a7530115ca32f969f0bccd6cf
Thom Nichols <tmnichols@gmail.com>
2013-07-29 16:16:01 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = 'oadr2-ven',
+    version = '0.1',
+    description = 'OpenADR 2.0a VEN for Python',
+    author = 'EnerNOC Advanced Technology',
+    author_email = 'tnichols@enernoc.com',
+    url = 'http://open.enernoc.com',
+    packages = find_packages('.', exclude=['*.tests']),
+    install_requires = ['lxml', 'sleekxmpp', 'dnspython'],
+    zip_safe = False,
+)
